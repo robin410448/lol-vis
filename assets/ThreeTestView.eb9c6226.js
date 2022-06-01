c8fcd60271dd7a15f9c6320e96c1f4419b1bf86d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-06-01 15:14:52 +0000

    Deploy ernestchu/lol-vis to github.com/ernestchu/lol-vis.git:gh-pages

--- assets/ThreeTestView.eb9c6226.js
+++ assets/ThreeTestView.eb9c6226.js
@@ -1,4 +1,4 @@
-import{as as dc,ay as fc,W as pc}from"./index.dd5a076d.js";/**
+import{as as dc,ay as fc,W as pc}from"./index.08d5e803.js";/**
  * @license
  * Copyright 2010-2022 Three.js Authors
  * SPDX-License-Identifier: MIT
