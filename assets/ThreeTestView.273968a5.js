cdad3f02c58a1e0eb0434a1c0d693038ab9ffa27
robin410448 <robin410448@users.noreply.github.com>
2022-06-06 16:15:57 +0000

    deploy: 2ca7869d6fca5b0475ab1f83fd8ade78378fdeb2

--- assets/ThreeTestView.273968a5.js
+++ assets/ThreeTestView.273968a5.js
@@ -1,4 +1,4 @@
-import{as as dc,ay as fc,W as pc}from"./index.8417a6d2.js";/**
+import{as as dc,ay as fc,W as pc}from"./index.c186efad.js";/**
  * @license
  * Copyright 2010-2022 Three.js Authors
  * SPDX-License-Identifier: MIT
